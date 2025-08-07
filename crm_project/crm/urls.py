5e9bf8faa92cbca1bbc65fbfba0f15f4948eae6a
Jessy <bryanjessywaweru@gmail.com>
2025-08-07 05:40:01 -0700

    :sparkles:Enabled JWT

--- crm_project/crm/urls.py
+++ crm_project/crm/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ClientViewSet, ProjectViewSet, InvoiceViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = DefaultRouter()
 router.register(r'clients', ClientViewSet)
@@ -9,4 +13,12 @@ router.register(r'invoices', InvoiceViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+]
+
+
+
+
+urlpatterns += [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- crm_project/crm/views.py
+++ crm_project/crm/views.py
@@ -2,18 +2,22 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Client, Project, Invoice
 from .serializers import ClientSerializer, ProjectSerializer, InvoiceSerializer
+from rest_framework.permissions import IsAuthenticated
 #viewsets.ModelViewSet gives full CRUD functionality (GET, POST, PUT, DELETE)
 #  without writing those methods manually.
 class ClientViewSet(viewsets.ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access
 
 class InvoiceViewSet(viewsets.ModelViewSet):
     queryset = Invoice.objects.all()
     serializer_class = InvoiceSerializer
+    permission_classes = [IsAuthenticated]  # Ensure only authenticated users can access
 
 # Create your views here.
