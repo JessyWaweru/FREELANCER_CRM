2f5be6a1df2005b08a11762fbcfb1a0b650cddcc
Jessy <bryanjessywaweru@gmail.com>
2025-10-03 02:37:05 -0700

    :construction:Added correct config for templates in settings.py

--- crm_project/settings.py
+++ crm_project/settings.py
@@ -108,16 +108,20 @@ SIMPLE_JWT = {
 }
 ROOT_URLCONF = 'crm_project.urls'
 
+
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "templates"],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [
+            BASE_DIR / "crm_project" / "templates",  
+        ],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
