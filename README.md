493db27a8d9fbdd89107a692d113001c49dd0219
Jessy <bryanjessywaweru@gmail.com>
2025-11-26 05:15:28 -0800

    :rocket:Made few changes to kickstart deployment

--- README.md
+++ README.md
@@ -197,3 +197,5 @@ Feel free to open issues or submit pull requests.
 This project is licensed under the **MIT License**.
 
 
+
+-
--- crm_project/settings.py
+++ crm_project/settings.py
@@ -91,12 +91,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",  
-    "http://127.0.0.1:5174",
-    "http://localhost:5173",  
-    "http://127.0.0.1:5173",
-]
+CORS_ALLOW_ALL_ORIGINS = True #tighten this in production
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
